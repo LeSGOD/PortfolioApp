c6803c5cdfade05604030c65bff62bb5f60618fe
Artur Leszczynski <artles00@wp.pl>
2020-06-10 23:28:36 +0200

    Django portfolio

--- projects/views.py
+++ projects/views.py
@@ -8,3 +8,8 @@ def all_projects(request):
     #query the db to return all projects objects
     projects = Project.objects.all()
     return render(request, 'projects/all_projects.html', {'projects': projects})
+
+ 
+def project_detail(request, pk):
+    project = Project.objects.get(pk=pk)
+    return render(request, 'projects/detail.html', {'project': project})
